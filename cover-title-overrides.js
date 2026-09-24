e3856060689d8c790dc523364c73f79a7c52ea8e
Blade1090 <jscott@kellytours.com>
2026-09-24 14:54:26 -0400

    Expand curated PS4 cover corrections

--- cover-title-overrides.js
+++ cover-title-overrides.js
@@ -2,5 +2,13 @@
 // These override automated GameEye/IGDB art when the automated source is generic/key art or the wrong physical presentation.
 window.SHELFCHECK_TITLE_COVERS={
   "a boy and his blob":"https://pnpdistribution.com/i/A-BOY-AN-P4-N.jpg",
-  "2dark":"https://vgdb.uk/images/db/covers/214244.jpg"
+  "2dark":"https://vgdb.uk/images/db/covers/214244.jpg",
+  "adk tamashii":"https://limitedrungames.com/cdn/shop/products/ADKProductImage.png?height=1080&v=1576795434",
+  "alchemic jousts":"https://www.vpd.fi/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/a/l/alchemic_joust_ps4_sp_po84179.jpg",
+  "alienation":"https://cdn.nguyenkimmall.com/images/detailed/232/dia-game--pcas00059-alienation.jpg",
+  "anima gate of memories the nameless chronicles":"https://gameline.ph/cdn/shop/files/w_02367a5f-0f95-4ffb-95f2-23e919b404ba_1200x1200.png?v=1759903314",
+  "ara fell enhanced edition":"https://www.tradeinn.com/f/14135/141350183/playstation-ps4-ara-fell-and-rise-of-the-third-power-import.webp",
+  "armagallant decks of destiny":"https://pnpdistribution.com/i/ARMAG-DD-P4-I.jpg",
+  "asdivine hearts":"https://limitedrungames.com/cdn/shop/products/AH-PS4.png?v=1503350384",
+  "atari flashback classics volume 1":"https://cdn.awsli.com.br/2500x2500/138/138431/produto/27132878/8e5fec5bad.jpg"
 };
