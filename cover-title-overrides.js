34e8d2d45d0195ede7f22414b7058d2ca1628762
Blade1090 <jscott@kellytours.com>
2026-09-24 14:32:57 -0400

    Add curated production cover corrections

---
+++ cover-title-overrides.js
@@ -0,0 +1,6 @@
+// ShelfCheck curator-approved physical PS4 front-cover corrections.
+// These override automated GameEye/IGDB art when the automated source is generic/key art or the wrong physical presentation.
+window.SHELFCHECK_TITLE_COVERS={
+  "a boy and his blob":"https://pnpdistribution.com/i/A-BOY-AN-P4-N.jpg",
+  "2dark":"https://vgdb.uk/images/db/covers/214244.jpg"
+};
